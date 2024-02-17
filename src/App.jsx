36c4e5bafac93c4cd16818dee1e3af26a286d271
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-02-17 18:47:15 +0300

    adaptive layout of all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { Routes, Route } from "react-router-dom";
-
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from "./page/Home";
-import { NotFound } from "./page/NotFound";
 
 function App() {
   return (
--- src/page/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export const NotFound = () => {
-  return (
-    <div>
-      Страница не найдена! <Link to="/">На главную</Link>
-    </div>
-  );
-};
